c8589dd8b9b603ab5257e154628690d945abd2d6
Laszer271 <Laszer2710@gmail.com>
2023-11-19 09:49:27 +0100

    update frontend

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-data = {
-    'Name of bank': ['Bank A', 'Bank B', 'Bank C', 'Bank D', 'Bank E'],
-    'Date': ['01-11-2023', '05-12-2023', '10-09-2023', '15-07-2023', '20-03-2023'],
-    'Product Type': ['Savings', 'Checking', 'Term Deposit', 'Savings', 'Savings'],
-    'Length/Maturity': ['1 year', 'Ongoing', '3 years', '30 years', '5 years'],
-    'Interest Rate': [2.5, 1.8, 3.2, 4.0, 5.5],
-    'Customer Type': ['Individual', 'Corporate', 'Individual', 'Individual', 'Corporate'],
-    'Offer Type': ['Promotional', 'Standard', 'Standard', 'Promotional', 'Standard'],
-    'Maximum Deposit Amount': [10000, 50000, 20000, 100000, 30000],
-    'Other Significant Conditions': ['N/A', 'Minimum balance required', 'N/A', 'Insurance required', 'N/A']
-}
-df = pd.DataFrame(data)
-
 
 def main():
+
+    path = "results.csv"
+    df = pd.pandas.read_csv(path)
+    
+    df['HighestInterest'] = df['HighestInterest'].abs()
+    df['Length'] = df['Length'].abs()
+    df['Length'] = df['Length'].apply(lambda x: x if x <= 36 else 36)
+    df['MaxPLN'] = df['MaxPLN'].apply(lambda x: x if x >= 10000 else 1000)
     
     st.markdown("<h1 style='text-align: center; color: White;background-color:#e84343'>InterestGuardian</h1>", unsafe_allow_html=True)
     # st.markdown("<h3 style='text-align: center; color: Black;'>Drop in The required Inputs and we will do  the rest.</h3>", unsafe_allow_html=True)
+
     # st.markdown("<h4 style='text-align: center; color: Black;'>Submission for The Python Week</h4>",
     #             unsafe_allow_html=True)
     st.header("What is this Project about?")
@@ -28,25 +24,30 @@ def main():
     st.sidebar.header("FILTERS")
 
     # taking the cgpa by giving in the range from 0 to 10
-    selected_banks = st.sidebar.multiselect("Filter by banks",df['Name of bank'].to_list(), df['Name of bank'].to_list())
-    selected_interest_threshold = st.sidebar.slider("Input your interest threshold", 0, 20)
+    selected_banks = st.sidebar.multiselect("Filter by banks",df['Bank'].unique().tolist(), df['Bank'].unique().tolist())
+    selected_interest_threshold = st.sidebar.slider("Input your interest threshold", 0, 15)
     # taking the TOEFL by giving in the range from 0 to 120
-    selected_deposit_threshold = st.sidebar.slider("Input your maximum deposit value", 10000, 100000)
+    selected_deposit_threshold = st.sidebar.slider("Input your minimum deposit value", 1, 100000)
     # taking the input of whether or not a person has written a research paper
 
-    c = dict(zip(df["Name of bank"].unique(), px.colors.qualitative.G10))
+    c = dict(zip(df["Bank"].unique(), px.colors.qualitative.G10))
 
 
-    filtered_df = df[df['Name of bank'].isin(selected_banks) & (df['Interest Rate'] >= selected_interest_threshold) & (df['Maximum Deposit Amount'] >= selected_deposit_threshold) & (df['Customer Type'] == 'Individual')]
-    fig = px.bar(filtered_df, x="Interest Rate", y="Maximum Deposit Amount", color="Name of bank",color_discrete_map=c, title = 'Indiviudal clients')
+    filtered_df = df[df['Bank'].isin(selected_banks) & (df['HighestInterest'] >= selected_interest_threshold) & (df['MaxPLN'] >= selected_deposit_threshold) & (df['Client type'] == 'Individual')]
+    
+    fig = px.bar(filtered_df, x="HighestInterest", y="MaxPLN", color="Bank",color_discrete_map=c, title = 'Indiviudal clients')
 
-    filtered_df = df[df['Name of bank'].isin(selected_banks) & (df['Interest Rate'] >= selected_interest_threshold) & (df['Maximum Deposit Amount'] >= selected_deposit_threshold) & (df['Customer Type'] == 'Corporate')]
-    fig_2 = px.bar(filtered_df, x="Interest Rate", y="Maximum Deposit Amount", color="Name of bank",color_discrete_map=c, title = 'Corporate clients')
+    filtered_df = df[df['Bank'].isin(selected_banks) & (df['HighestInterest'] >= selected_interest_threshold) & (df['MaxPLN'] >= selected_deposit_threshold) & (df['Client type'] == 'Corporation')]
+
+    fig_2 = px.bar(filtered_df, x="HighestInterest", y="MaxPLN", color="Bank",color_discrete_map=c, title = 'Corporation ')
+
+    fig_3 = px.bar(filtered_df, x='Bank', y="HighestInterest", color="Bank",color_discrete_map=c, title = 'Interest Rate')
  
 
     # Plot!
     st.plotly_chart(fig, use_container_width=True)
     st.plotly_chart(fig_2, use_container_width=True)
+    st.plotly_chart(fig_3, use_container_width=True)
 
 
         # if st.button('Predict'):  # making and printing our prediction
